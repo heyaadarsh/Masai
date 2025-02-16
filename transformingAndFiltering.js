ffc2e7c2229030d33976df961e227b12723ff867
Aadarsh Giri <93307777+heyaadarsh@users.noreply.github.com>
2025-02-16 18:33:21 +0530

    Create transformingAndFiltering.js

---
+++ transformingAndFiltering.js
@@ -0,0 +1,18 @@
+function processProducts(products) {
+  const productNames = products.map(product => product.name);
+
+  products.forEach(product => {
+    const message = product.price > 50 ? "above" : "below";
+    console.log(`${product.name} is ${message} $50`);
+  });
+
+  return productNames;
+}
+
+// Example :
+const products = [
+  { name: "Laptop", price: 1000 },
+  { name: "Mouse", price: 20 }
+];
+
+console.log(processProducts(products));
